ff51d865613e458358257ba570d8baec4ced5930
aadam11176 <99559452+aadam11176@users.noreply.github.com>
2022-06-12 22:12:40 +0200

    Update Data_Generator.py
    add comment

--- Data_Generator.py
+++ Data_Generator.py
@@ -12,38 +12,41 @@ class Datagenerateor:
     #               3 -> all columns same type (float)
     #               4 -> all columns same type (string) 
     #               5 -> all columns same type (time) 
-    def __init__(self, number_of_columns, number_of_rows, randomtype_of_data = 1 ):
+    def __init__(self, number_of_columns, number_of_rows, generator_type = 1 ):
         
         self.number_of_columns = number_of_columns
         self. number_of_rows = number_of_rows
-        self.randomtype_of_data = randomtype_of_data
+        self.generator_type = generator_type
         self.data = self.Maingenerateor()
+        self.mini = 0
+        self.maxi = 100
         
-        
+    # the main generator: depending on the type it will create the data set 
     def Maingenerateor(self):
         col_name = [str(i) for i in range (self.number_of_columns)]
         df = pd.DataFrame()
-        if (self.randomtype_of_data == 1): # all columns are random type
+        if (self.generator_type == 1): # all columns are random type
             for x in col_name:
-                df[x] = self.GenrateColumsData(randint(2,3), 0, 100) #change 2 to 5
+                df[x] = self.GenrateColumsData(randint(2,3), self.mini, self.maxi) #change 2 to 5
         
-        if (self.randomtype_of_data == 2): # all columns are int
+        if (self.generator_type == 2): # all columns are int
             for x in col_name:   
-                df[x] = self.GenrateColumsData(2, 0, 100)
+                df[x] = self.GenrateColumsData(2, self.mini, self.maxi)
         
-        if (self.randomtype_of_data == 3): # all columns are float
+        if (self.generator_type == 3): # all columns are float
            for x in col_name:
-                df[x] = self.GenrateColumsData(3, 0, 100)
+                df[x] = self.GenrateColumsData(3, self.mini, self.maxi)
         
-        if (self.randomtype_of_data == 4): # all columns are string
+        if (self.generator_type == 4): # all columns are string
            for x in col_name: 
-                df[x] = self.GenrateColumsData(4, 0, 100)
+                df[x] = self.GenrateColumsData(4, self.mini, self.maxi)
         
-        if (self.randomtype_of_data == 5): # all columns are time
+        if (self.generator_type == 5): # all columns are time
            for x in col_name: 
-                df[x] = self.GenrateColumsData(5, 0, 100)
+                df[x] = self.GenrateColumsData(5, self.mini, self.maxi)
         return df
         
+    # help function for creating each row
     def GenrateColumsData(self, typ, mini, maxi):
         if (typ == 2): # int
             return  self.generate_random_int(mini, maxi)
@@ -54,7 +57,7 @@ class Datagenerateor:
         if (typ == 5): # time
             return self.generate_random_time(mini, maxi)
         
-            
+    # very simple help functions to create random data
         
     def generate_random_float(self, mini, maxi, uniform = True): # uniform = false -> normal distruibution
        if (uniform):
@@ -65,10 +68,10 @@ class Datagenerateor:
     
     def generate_random_int(self, mini, maxi):
         return np.random.randint(mini,maxi,self.number_of_rows)
-    
+    # to do 
     def generate_random_time(self, mini, maxi):
         pass
-    
+    # to do
     def generate_random_string(self, min_length, max_length):
         pass
   
